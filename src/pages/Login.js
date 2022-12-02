8249b3a64c26ebbe735894fb3234b21d8017f017
iambeto <rscabrera@gmail.com>
2022-12-02 13:51:57 -0600

    Login and Logout

---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Login() {
+    retrun (
+        <div>
+            Login
+        </div>
+    );
+}
+
+export default Login;
---
+++ src/pages/Logout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Logout() {
+    retrun (
+        <div>
+            Logout
+        </div>
+    );
+}
+
+export default Logout;
